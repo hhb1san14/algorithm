c620a36243b455d698340a5a593cdc0c11d671e3
huanghongbo <huanghongbo@baiwang.com>
2019-09-24 15:49:48 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# lintcode
-领扣
+### 项目介绍
+领扣的题，从简单开始，算自己的学习过程
+
+### 项目代码仓库
+https://github.com/hhb1san14/lintcode
+
+#### 克隆项目
+git clone https://github.com/hhb1san14/lintcode.git
+
+### 开发环境
+Spring Boot  2.1.7.RELEASE
+Java         1.8
---
+++ src/main/java/com/hhb/lintcode/LintcodeApplication.java
@@ -0,0 +1,13 @@
+package com.hhb.lintcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LintcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LintcodeApplication.class, args);
+	}
+
+}
