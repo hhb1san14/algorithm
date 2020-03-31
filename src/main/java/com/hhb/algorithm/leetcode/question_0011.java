47e32444942096c7f85fbbb7440eec84802935c2
huanghongbo <huanghongbo@baiwang.com>
2020-03-31 10:29:11 +0800

    增加备注

--- src/main/java/com/hhb/algorithm/leetcode/question_0011.java
+++ src/main/java/com/hhb/algorithm/leetcode/question_0011.java
@@ -3,7 +3,7 @@ package com.hhb.algorithm.leetcode;
 /**
  * @author: huanghongbo
  * @Date: 2020-03-31 10:00
- * @Description: https://leetcode-cn.com/problems/container-with-most-water/
+ * @Description: 盛最多水的容器:https://leetcode-cn.com/problems/container-with-most-water/
  * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
  * <p>
  * 说明：你不能倾斜容器，且 n 的值至少为 2。
--- src/main/java/com/hhb/algorithm/leetcode/question_0026.java
+++ src/main/java/com/hhb/algorithm/leetcode/question_0026.java
@@ -3,7 +3,9 @@ package com.hhb.algorithm.leetcode;
 /**
  * @author: huanghongbo
  * @Date: 2020-03-30 16:53
- * @Description: 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ * @Description: 删除排序数组中的重复项:https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+ * <p>
+ * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
  * <p>
  * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
  * <p>
