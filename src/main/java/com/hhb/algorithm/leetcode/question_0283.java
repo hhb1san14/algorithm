22732737e47ab3f44b04fb90124bca3e1fb38e08
huanghongbo <huanghongbo@baiwang.com>
2020-05-11 18:49:30 +0800

    移动零

---
+++ src/main/java/com/hhb/algorithm/leetcode/question_0283.java
@@ -0,0 +1,42 @@
+package com.hhb.algorithm.leetcode;
+
+/**
+ * @author: huanghongbo
+ * @Date: 2020-05-11 18:39
+ * @Description: 移动零 https://leetcode-cn.com/problems/move-zeroes/
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ * <p>
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ */
+public class question_0283 {
+
+
+    public static void moveZeroes(int[] nums) {
+        int index = 0;
+        int temp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                temp = nums[index];
+                nums[index] = nums[i];
+                nums[i] = temp;
+                index++;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+        moveZeroes(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.err.print(arr[i] + " == >> ");
+        }
+    }
+}
