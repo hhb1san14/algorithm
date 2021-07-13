5722d1f48d71eb5cae2a367e85548418722dc916
huanghongbo <huanghongbo@baiwang.com>
2021-07-13 14:15:35 +0800

    环形链表 II

---
+++ src/main/java/com/hhb/algorithm/leetcode/question_0142.java
@@ -0,0 +1,91 @@
+package com.hhb.algorithm.leetcode;
+
+/**
+ * @description:142. 环形链表 II https://leetcode-cn.com/problems/linked-list-cycle-ii/
+ * <p>
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ * <p>
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ * 如果 pos 是 -1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+ * <p>
+ * 说明：不允许修改给定的链表。
+ * <p>
+ * 进阶：
+ * <p>
+ * 你是否可以使用 O(1) 空间解决此题？
+ *  
+ * <p>
+ * 示例 1： 3 -> 2 -> 0 -> -4 -> 2
+ * <p>
+ * <p>
+ * <p>
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：返回索引为 1 的链表节点
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * 示例 2： 1 -> 2 -> 1
+ * <p>
+ * <p>
+ * <p>
+ * 输入：head = [1,2], pos = 0
+ * 输出：返回索引为 0 的链表节点
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * 示例 3：1
+ * <p>
+ * <p>
+ * 输入：head = [1], pos = -1
+ * 输出：返回 null
+ * 解释：链表中没有环。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 链表中节点的数目范围在范围 [0, 104] 内
+ * -105 <= Node.val <= 105
+ * pos 的值为 -1 或者链表中的一个有效索引
+ * @author: huanghongbo
+ * @date: 2021-07-13 13:38
+ **/
+public class question_0142 {
+
+    public static ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode flow = head;
+        boolean flag = false;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            flow = flow.next;
+            if (fast == flow) {
+                flag = true;
+                break;
+            }
+        }
+        if(!flag){
+            return null;
+        }
+        flow = head;
+        while (flow != fast) {
+            fast = fast.next;
+            flow = flow.next;
+        }
+        return flow;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+//        ListNode listNode3 = new ListNode(3);
+//        ListNode listNode4 = new ListNode(4);
+//        ListNode listNode5 = new ListNode(5);
+        listNode1.next = listNode2;
+//        listNode2.next = listNode3;
+//        listNode3.next = listNode4;
+//        listNode4.next = listNode5;
+//        listNode5.next = listNode3;
+        ListNode listNode = detectCycle(listNode1);
+
+    }
+}
