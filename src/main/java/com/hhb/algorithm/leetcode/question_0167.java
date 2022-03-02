b0692d80fc399cf538ac5b0fae22a3bd29b1f0b3
hhb1san14 <hhb1san14@163.com>
2022-03-02 21:25:27 +0800

    复习双指针

--- src/main/java/com/hhb/algorithm/leetcode/question_0167.java
+++ src/main/java/com/hhb/algorithm/leetcode/question_0167.java
@@ -68,9 +68,25 @@ public class question_0167 {
         throw new IllegalArgumentException("Failed");
     }
 
+
+    public static int[] twoSum3(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            if (target < numbers[left] + numbers[right]) {
+                right--;
+            } else if (target > numbers[left] + numbers[right]) {
+                left++;
+            } else {
+                return new int[]{left + 1, right + 1};
+            }
+        }
+        return new int[]{};
+    }
+
     public static void main(String[] args) {
         int[] arr = {2, 7, 11, 15};
-        int[] ints = twoSum2(arr, 9);
+        int[] ints = twoSum3(arr, 9);
         for (int i = 0; i < ints.length; i++) {
             System.err.println(ints[i]);
 
