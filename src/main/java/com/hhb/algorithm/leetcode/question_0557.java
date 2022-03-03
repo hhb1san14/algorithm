a1da67ebbfc69c92e5f161d4efc379f612aa5b90
hhb1san14 <hhb1san14@163.com>
2022-03-03 21:14:29 +0800

    反转字符串中的单词 III

---
+++ src/main/java/com/hhb/algorithm/leetcode/question_0557.java
@@ -0,0 +1,47 @@
+package com.hhb.algorithm.leetcode;
+
+/**
+ * @author hhb
+ * @date 2022/3/3
+ * @description557. 反转字符串中的单词 III https://leetcode-cn.com/problems/reverse-words-in-a-string-iii/
+ * 给定一个字符串 s ，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：s = "Let's take LeetCode contest"
+ * 输出："s'teL ekat edoCteeL tsetnoc"
+ * 示例 2:
+ *
+ * 输入： s = "God Ding"
+ * 输出："doG gniD"
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 5 * 104
+ * s 包含可打印的 ASCII 字符。
+ * s 不包含任何开头或结尾空格。
+ * s 里 至少 有一个词。
+ * s 中的所有单词都用一个空格隔开。
+ *
+ */
+public class question_0557 {
+
+    public String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder("");
+        String[] arr = s.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(reverse(arr[i]));
+            if(i < arr.length - 1){
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+    public String reverse(String str){
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
+    }
+}
