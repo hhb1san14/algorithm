9c5eb1be9a1ec4aedddfde7a71090027bd831495
hhb1san14 <hhb1san14@163.com>
2022-04-24 21:37:05 +0800

    全排列II

---
+++ src/main/java/com/hhb/algorithm/leetcode/question_0047.java
@@ -0,0 +1,78 @@
+package com.hhb.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author hhb
+ * @date 2022/4/23
+ * @description 47. 全排列 II https://leetcode-cn.com/problems/permutations-ii/
+ * 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,1,2]
+ * 输出：
+ * [[1,1,2],
+ * [1,2,1],
+ * [2,1,1]]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 8
+ * -10 <= nums[i] <= 10
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/permutations-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class question_0047 {
+
+
+    private boolean[] vis;
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        vis = new boolean[nums.length];
+        Arrays.sort(nums);
+        permuteUnique(nums, list, temp);
+        return list;
+    }
+
+    private void permuteUnique(int[] nums, List<List<Integer>> list, List<Integer> temp) {
+        if (temp.size() == nums.length) {
+            list.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (vis[i] || (i > 0 && nums[i] == nums[i - 1] && !vis[i - 1])) {
+                continue;
+            }
+            temp.add(nums[i]);
+            vis[i] = true;
+            permuteUnique(nums, list, temp);
+            vis[i] = false;
+            temp.remove(temp.size() - 1);
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        question_0047 question_0047 = new question_0047();
+        List<List<Integer>> lists = question_0047.permuteUnique(new int[]{1, 2, 1, 1});
+        System.err.println(lists);
+    }
+
+
+}
