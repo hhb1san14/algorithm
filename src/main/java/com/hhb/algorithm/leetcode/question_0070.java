91e37355213c6c8e8c1ac4a95a57f14a449249c2
hhb1san14 <hhb1san14@163.com>
2022-06-20 21:51:22 +0800

    爬楼梯-复习

--- src/main/java/com/hhb/algorithm/leetcode/question_0070.java
+++ src/main/java/com/hhb/algorithm/leetcode/question_0070.java
@@ -60,8 +60,53 @@ public class question_0070 {
         return arr[n];
     }
 
-    public static void main(String[] args) {
+    //超出时间限制，结果正确 // N = 45  RESULT = 1836311903
+    public static int climbStairs3(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        return climbStairs3(n - 1) + climbStairs3(n - 2);
+    }
 
+    // N = 45  RESULT = 1836311903
+    public static int climbStairs4(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] arr = new int[n + 1];
+        arr[1] = 1;
+        arr[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+
+    /**
+     * n = 1 ，result = 1
+     * n = 2 ，result = 2
+     * n = 3 , result = f(1) + f(2) = f(n - 1) + f(n -2);
+     *
+     * @param n
+     * @return
+     */
+    public static int climbStairs5(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int result = 0;
+        int left = 1;
+        int right = 2;
+        for (int i = 3; i <= n; i++) {
+            result = left + right;
+            left = right;
+            right = result;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.err.println(climbStairs5(45));
 
     }
 }
