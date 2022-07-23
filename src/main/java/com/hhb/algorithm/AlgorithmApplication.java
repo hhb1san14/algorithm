b9f642c993e63c402d0e189b8e8fb875585f68ec
huanghongbo <hhb1san14@163.com>
2022-07-23 23:20:30 +0800

    test

--- src/main/java/com/hhb/algorithm/AlgorithmApplication.java
+++ src/main/java/com/hhb/algorithm/AlgorithmApplication.java
@@ -3,9 +3,7 @@ package com.hhb.algorithm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * test
- */
+
 @SpringBootApplication
 public class AlgorithmApplication {
 
