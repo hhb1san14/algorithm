424105cf4a56c935feb2ec7e2822056e8405493a
huanghongbo <hhb1san14@163.com>
2022-07-23 23:46:39 +0800

    t

--- src/main/java/com/hhb/algorithm/AlgorithmApplication.java
+++ src/main/java/com/hhb/algorithm/AlgorithmApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AlgorithmApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(AlgorithmApplication.class, args);
     }
